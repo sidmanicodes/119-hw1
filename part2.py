ff0c95d62df80f7eb2fe62d8de091b2543814740
Caleb Stanford <cdstanford@ucdavis.edu>
2024-10-16 09:10:51 -0700

    :wrench: part 2 q11-15

--- part2.py
+++ part2.py
@@ -3,11 +3,12 @@ Part 2: Performance Comparisons
 
 **Released: Wednesday, October 16**
 
-In this part, each question will ask you to compare
-two or more versions of a pipeline to determine which is the fastest.
-
-You will compare the throughput and latency of each version of the pipeline
-and report which one is faster.
+In this part, we will explore comparing the performance
+of different pipelines.
+First, we will set up some helper classes.
+Then we will do a few comparisons
+between two or more versions of a pipeline
+to report which one is faster.
 """
 
 import part1
@@ -26,10 +27,14 @@ size is the number of elements in the input dataset for the pipeline,
 and func is a function that can be run on zero arguments
 which runs the pipeline (like def f()).
 
+The second is a similar helper class for latency (Q3).
+
 Fill in the add_pipeline, eval_throughput, and generate_plot functions below.
 """
 
 # Number of times to run each pipeline in the following results.
+# You may modify this if any of your tests are running particularly slow
+# or fast (though it should be at least 10).
 NUM_RUNS = 1000
 
 class ThroughputHelper:
@@ -58,7 +63,8 @@ class ThroughputHelper:
     def compare_throughput(self):
         # Measure the throughput of all pipelines
         # and store it in a list in self.throughputs.
-        # Also, return the throughput as a string.
+        # Make sure to use the NUM_RUNS variable.
+        # Also, return the resulting list of throughputs.
         raise NotImplementedError
 
     def generate_plot(self, filename):
@@ -70,13 +76,13 @@ class ThroughputHelper:
         raise NotImplementedError
 
 """
-1b. As your answer to this part,
-return the number of times you ran the pipeline in the above helper class
-in each call to eval_throughput, per pipeline.
+As your answer to this part,
+return a string containing the plot method you decided to use in
+matplotlib.
 """
 
-def q1b():
-    # Return the number of times you ran the pipeline.
+def q1():
+    # Return plot method (as a string) from matplotlib
     raise NotImplementedError
 
 """
@@ -89,12 +95,13 @@ We will compare three versions of the pipeline depending on the
 input size.
 """
 
-LIST_SMALL = [10] * 1000
+LIST_SMALL = [10] * 100
 LIST_MEDIUM = [10] * 100_000
-LIST_LARGE = [10] * 10_000_000
+LIST_LARGE = [10] * 100_000_000
 
 def add_list(l):
     # TODO
+    # Please use a for loop (not a built-in)
     raise NotImplementedError
 
 def q2a():
@@ -102,9 +109,13 @@ def q2a():
     h = ThroughputHelper()
     # Add the 3 pipelines.
     # (You will need to create a pipeline for each one.)
+    # Pipeline names: small, medium, large
     raise NotImplementedError
-    # Finally, return the throughput and generate a plot.
+    # Generate a plot.
     # Save the plot as 'output/q2a.png'.
+    # TODO
+    # Finally, return the throughputs as a list.
+    # TODO
 
 """
 2b.
@@ -150,7 +161,7 @@ class LatencyHelper:
     def compare_latency(self):
         # Measure the latency of all pipelines
         # and store it in a list in self.latencies.
-        # Also, return the latency as a string.
+        # Also, return the resulting list of latencies.
         raise NotImplementedError
 
     def generate_plot(self, filename):
@@ -162,12 +173,12 @@ class LatencyHelper:
         raise NotImplementedError
 
 """
-3b. As your answer to this part,
+As your answer to this part,
 return the number of input items that each pipeline should
 process if the class is used correctly.
 """
 
-def q3b():
+def q3():
     # Return the number of input items in each dataset,
     # for the latency helper to run correctly.
     raise NotImplementedError
@@ -175,6 +186,12 @@ def q3b():
 """
 4. To make sure your monitor is working, test it on
 the simple pipeline from Q2.
+
+For latency, all three pipelines would only process
+one item. Therefore instead of using
+LIST_SMALL, LIST_MEDIUM, and LIST_LARGE,
+for this question run the same pipeline three times
+on a single list item.
 """
 
 LIST_SINGLE_ITEM = [] # TODO: fill in this line
@@ -182,19 +199,18 @@ LIST_SINGLE_ITEM = [] # TODO: fill in this line
 def q4a():
     # Create a LatencyHelper object
     h = LatencyHelper()
-    # Add the single pipeline
-    # Finally, return the throughput and generate a plot.
-    # Save the plot as 'output/q4a.png'.
+    # Add the single pipeline three times.
     raise NotImplementedError
+    # Generate a plot.
+    # Save the plot as 'output/q2a.png'.
+    # TODO
+    # Finally, return the latencies as a list.
+    # TODO
 
 """
-Q4b.
-If you were to run all three pipelines in Q2,
-which pipeline would have the lowest latency?
-
-(This is a bit of a trick question!
-Remember that latency is measured as the time
-it takes to process one item only.)
+4b.
+How much did the latency vary between the three copies of the pipeline?
+Is this more or less than what you expected?
 
 === ANSWER Q1b BELOW ===
 
@@ -202,15 +218,20 @@ it takes to process one item only.)
 """
 
 """
+Now that we have our helpers, let's do a simple comparison.
+
+NOTE: you may add other helper functions that you may find useful
+as you go through this file.
+
 5. Comparison on Part 1
 
 Finally, use the helpers above to calculate the throughput and latency
 of the pipeline in part 1.
 """
 
-# You should need both of these
-from part1 import load_input
-from part1 import PART_1_PIPELINE
+# You will need these:
+# part1.load_input
+# part1.PART1_PIPELINE
 
 def q5a():
     # Return the throughput of the pipeline in part 1.
@@ -221,16 +242,213 @@ def q5b():
     raise NotImplementedError
 
 """
-===== Questions 6-10: Performance Comparisons =====
+===== Questions 6-10: Performance Comparison 1 =====
 
-Next, we will look at a series of performance comparisons
-to compare:
-- the cost of getting input from a file, vs. in an existing DataFrame
-- the cost of vectorization (compared to for loops)
-- the cost of explicit transformations vs. SQL
-- the cost of using different encodings (like one-hot encoding)
-  and encodings for null values.
+For our first performance comparison,
+let's look at the cost of getting input from a file, vs. in an existing DataFrame.
+
+6. We will use the same population dataset
+that we used in lecture 3.
+
+Load the data using load_input() given the file name.
+Then, set up a simple pipeline that returns
+a 5-number summary for the population data.
+
+Make sure that it cleans the data by removing continents
+and world data first!
+(World data is listed under OWID_WRL)
 """
+
+def load_input(filename):
+    # Return a dataframe containing the population data
+    # **Clean the data here**
+    raise NotImplementedError
+
+def population_pipeline(df):
+    # Input: the dataframe from load_input()
+    # Return a 5-number summary (as a list of 5 elements)
+    raise NotImplementedError
+
+def q6():
+    # As your answer to this part,
+    # call load_input() and then population_pipeline()
+    # and return the 5-number summary.
+    raise NotImplementedError
+
+"""
+7. Varying the input size
+
+Next we want to set up three different datasets of different sizes.
+
+Create three new files,
+    - data/population-small.csv
+      with the first 600 rows
+    - data/population-medium.csv
+      with the first 6000 rows
+    - data/population-single-row.csv
+      with only the first row
+      (for calculating latency)
+
+You can edit the csv file directly to extract the first rows
+(remember to also include the header row)
+and save a new file.
+
+Make four versions of load input that load your datasets.
+(The _large one should use the full population dataset.)
+"""
+
+def load_input_small():
+    raise NotImplementedError
+
+def load_input_medium():
+    raise NotImplementedError
+
+def load_input_large():
+    raise NotImplementedError
+
+def load_input_single_row():
+    # This is the pipeline we will use for latency.
+    raise NotImplementedError
+
+def q7():
+    # Don't modify this part
+    s = load_input_small()
+    m = load_input_medium()
+    l = load_input_large()
+    x = load_input_single_row()
+    return [len(s), len(m), len(l), len(x)]
+
+"""
+8.
+Create baseline pipelines
+
+First let's create our baseline pipelines.
+Create four pipelines,
+    baseline_small
+    baseline_medium
+    baseline_large
+    baseline_latency
+
+based on the three datasets above.
+Each should call your population_pipeline from Q7.
+"""
+
+def baseline_small():
+    raise NotImplementedError
+
+def baseline_medium():
+    raise NotImplementedError
+
+def baseline_large():
+    raise NotImplementedError
+
+def baseline_latency():
+    raise NotImplementedError
+
+def q8():
+    # Don't modify this part
+    _ = baseline_medium()
+    return ["baseline_small", "baseline_medium", "baseline_large", "baseline_latency"]
+
+"""
+9.
+Finally, let's compare whether loading an input from file is faster or slower
+than getting it from an existing Pandas dataframe variable.
+
+Create four new dataframes (constant global variables)
+directly in the script.
+Then use these to write 3 new pipelines:
+    fromvar_small
+    fromvar_medium
+    fromvar_large
+    fromvar_latency
+
+As your answer to this part;
+a. Generate a plot in output/q9a.png of the throughputs
+    Return the list of 6 throughputs in this order:
+    baseline_small, baseline_medium, baseline_large, fromvar_small, fromvar_medium, fromvar_large
+b. Generate a plot in output/q9b.png of the latencies
+    Return the list of 2 latencies in this order:
+    baseline_latency, fromvar_latency
+"""
+
+# TODO
+# POPULATION_SMALL =
+# POPULATION_MEDIUM =
+# POPULATION_LARGE =
+# POPULATION_SINGLE_ROW =
+
+def fromvar_small():
+    raise NotImplementedError
+
+def fromvar_medium():
+    raise NotImplementedError
+
+def fromvar_large():
+    raise NotImplementedError
+
+def fromvar_latency():
+    raise NotImplementedError
+
+def q9a():
+    # Add all 6 pipelines for a throughput comparison
+    # Generate plot in ouptut/q9a.png
+    # Return list of 6 throughputs
+    raise NotImplementedError
+
+def q9b():
+    # Add 2 pipelines for a latency comparison
+    # Generate plot in ouptut/q9b.png
+    # Return list of 2 latencies
+    raise NotImplementedError
+
+"""
+10.
+Comment on the plots above!
+How dramatic is the difference between the two pipelines?
+What does this experiment show?
+
+===== ANSWER Q10 BELOW =====
+
+===== END OF Q10 ANSWER =====
+"""
+
+"""
+===== Questions 11-15: Performance Comparison 2 =====
+
+Our second performance comparison will explore vectorization.
+
+Operations in Pandas use Numpy arrays and vectorization to enable
+fast operations.
+In particular, they are often much faster than using for loops.
+
+Let's explore whether this is true!
+"""
+
+
+"""
+===== Extra Credit =====
+
+Use your pipeline to compare something else!
+
+Here are some ideas for what to try:
+- the cost of random sampling vs. the cost of getting rows from the
+  DataFrame manually
+- the cost of cloning a DataFrame
+- the cost of sorting a DataFrame prior to doing a computation
+- the cost of using different encodings (like one-hot encoding)
+  and encodings for null values
+- the cost of querying via Pandas methods vs querying via SQL
+  For this part: you would want to use something like
+  pandasql that can run SQL queries on Pandas data frames. See:
+  https://stackoverflow.com/a/45866311/2038713
+
+As your answer to this part,
+as before, return the list of 6 throughputs
+and the list of 6 latencies.
+"""
+
+
 
 """
 ===== Wrapping things up =====
@@ -262,11 +480,23 @@ def log_answer(name, func, *args):
 def PART_2_PIPELINE():
     open(ANSWER_FILE, 'w').close()
 
-    try:
-        dfs = part1.load_input()
-    except NotImplementedError:
-        print("load_input() not found; did you complete part 1?")
-        dfs = []
+    # Q1-5
+    log_answer("q1", q1)
+    log_answer("q2a", q2a)
+    # 2b: commentary
+    log_answer("q3", q3)
+    log_answer("q4a", q4a)
+    # 4b: commentary
+    log_answer("q5a", q5a)
+    log_answer("q5b", q5b)
+
+    # Q6-10
+    log_answer("q6", q6)
+    log_answer("q7", q7)
+    log_answer("q8", q8)
+    log_answer("q9a", q9a)
+    log_answer("q9b", q9b)
+    # 10: commentary
 
     # Answer: return the number of questions that are not implemented
     if UNFINISHED > 0:
